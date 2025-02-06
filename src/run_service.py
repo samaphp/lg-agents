7f2cf134dd9e7c8d47f2cc5d84a61ca82981c62b
Info APSquared <info@apsquared.co>
2025-02-05 21:18:54 -0500

    service

--- src/run_service.py
+++ src/run_service.py
@@ -8,7 +8,7 @@ from core import settings
 
 load_dotenv()
 
-VERSION = "0.0.91"
+VERSION = "0.1.0"
 
 def handle_shutdown(signum, frame):
     print("\nReceived shutdown signal. Exiting gracefully...")
