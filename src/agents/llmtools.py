773b608ca682a8fba23c1e4a30e821cea661d269
Info APSquared <info@apsquared.co>
2025-02-20 22:42:30 -0500

    updates to make it work for service

--- src/agents/llmtools.py
+++ src/agents/llmtools.py
@@ -2,7 +2,7 @@
 from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0) 
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.3) 
 groq_llm = ChatGroq(model="deepseek-r1-distill-llama-70b", temperature=0.2)
 
 def get_llm():
